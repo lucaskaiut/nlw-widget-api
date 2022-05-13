9ee2ff4852024a980a5623e11cc745c998783412
Lucas Kaiut <kaiutdesouza042@gmail.com>
2022-05-13 17:56:58 -0300

    changed backend port

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
-    console.log('HTTP Server running on port 3333');
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log(`HTTP Server running on port ${port}`);
 });
